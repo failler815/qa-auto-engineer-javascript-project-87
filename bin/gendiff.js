c21108ffa921e5ce5d3c781222a6cf5caa352fcc
Vyacheslav <failler815@gmail.com>
2024-09-24 22:51:20 +0300

    шаг 1 фикс

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((firstConfig, secondConfig) => {
+    console.log(genDiff(firstConfig, secondConfig, program.opts().format));
+  });
+
+program.parse();
