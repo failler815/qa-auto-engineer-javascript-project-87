8102e7ccc19e7f1dffa81960ef0e4c6b9e5578e3
Vyacheslav <failler815@gmail.com>
2024-10-09 16:16:36 +0300

    index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
+
 const genDiff = (data1 = {}, data2 = {}) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
-  const sortedKeys = keys.sort();
+  const sortKeys = keys.sort();
   let result = '{\n';
-  for (const key of sortedKeys) {
+  sortKeys.forEach((key) => {
     if (!Object.hasOwn(data1, key)) {
       result += `  + ${key}: ${data2[key]}\n`;
     } else if (!Object.hasOwn(data2, key)) {
@@ -16,7 +17,7 @@ const genDiff = (data1 = {}, data2 = {}) => {
     } else {
       result += `    ${key}: ${data1[key]}\n`;
     }
-  }
+  });
   result += '}';
   return result;
 };
