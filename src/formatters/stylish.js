638bed295cc731046efedf2cc1948f0f75eec0db
Vyacheslav <failler815@gmail.com>
2024-11-05 01:25:40 +0300

    fix4

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,27 +16,26 @@ const stringify = (value, depth, replacer = '    ') => {
 const stylish = (diffTree, replacer = '    ', depth = 1) => {
   const initialIndent = replacer.repeat(depth).slice(2);
   const result = diffTree.flatMap(({
-    key, value, status, oldValue, newValue, children
+    key, value, status, oldValue, newValue, children,
   }) => {
-      switch (status) {
-        case 'nested':
-          return `${initialIndent}  ${key}: ${stringify(children, replacer, depth + 1)}`;
-        case 'added':
-          return `${initialIndent}+ ${key}: ${stringify(value, depth)}`;
-        case 'deleted':
-          return `${initialIndent}- ${key}: ${stringify(value, depth)}`;
-        case 'changed':
-          return [
-            `${initialIndent}- ${key}: ${stringify(oldValue, depth)}`,
-            `${initialIndent}+ ${key}: ${stringify(newValue, depth)}`,
-          ];
-        case 'unchanged':
-          return `${initialIndent}  ${key}: ${stringify(value, depth)}`;
-        default:
-          throw new Error(`Unknown status: '${status}'!`);
-      }
+    switch (status) {
+      case 'nested':
+        return `${initialIndent}  ${key}: ${stringify(children, replacer, depth + 1)}`;
+      case 'added':
+        return `${initialIndent}+ ${key}: ${stringify(value, depth)}`;
+      case 'deleted':
+        return `${initialIndent}- ${key}: ${stringify(value, depth)}`;
+      case 'changed':
+        return [
+          `${initialIndent}- ${key}: ${stringify(oldValue, depth)}`,
+          `${initialIndent}+ ${key}: ${stringify(newValue, depth)}`,
+        ];
+      case 'unchanged':
+        return `${initialIndent}  ${key}: ${stringify(value, depth)}`;
+      default:
+        throw new Error(`Unknown status: '${status}'!`);
     }
-  );
+  });
   const outRepeat = replacer.repeat(depth - 1);
   return ['{', ...result, `${outRepeat}}`].join('\n');
 };
